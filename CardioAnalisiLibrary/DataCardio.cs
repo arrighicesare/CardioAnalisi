0d67f7d0164555c5f200030b04fdc8f99b16584e
ITT Rosselli Rasetti <rosselli.rasetti@outlook.com>
2019-04-08 10:42:26 +0200

    Primi Test

--- CardioAnalisiLibrary/DataCardio.cs
+++ CardioAnalisiLibrary/DataCardio.cs
@@ -8,8 +8,22 @@ namespace CardioAnalisiLibrary
 {
     public class DataCardio
     {
-
-
-
+        public static string Met_ContInsDat(string Eta)
+        {
+            string msg = "";
+            try
+            {
+                int eta = Convert.ToInt32(Eta);
+                if(eta<=0||eta>=150)
+                {
+                    msg = "ERRORE!!";
+                }
+            }
+            catch
+            {
+                msg = "ERRORE!!";
+            }
+            return msg;
+        }
     }
 }
--- DataCardio.Test/UnitTest1.cs
+++ DataCardio.Test/UnitTest1.cs
@@ -9,6 +9,18 @@ namespace DataCardio.Test
         [TestMethod]
         public void TestMethod1()
         {
+            string Eta = "0";
+            string Atteso = "ERRORE!!";
+            string Ottenuto = CardioAnalisiLibrary.DataCardio.Met_ContInsDat(Eta);
+            Assert.AreEqual(Atteso, Ottenuto);
+        }
+        [TestMethod]
+        public void TestMethod2()
+        {
+            string Eta = "150";
+            string Atteso = "ERRORE!!";
+            string Ottenuto = CardioAnalisiLibrary.DataCardio.Met_ContInsDat(Eta);
+            Assert.AreEqual(Atteso, Ottenuto);
         }
     }
 }
