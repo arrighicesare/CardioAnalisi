3c15655285553fa0c85ef1256b624e43b03a570d
arrighicesare <46019616+arrighicesare@users.noreply.github.com>
2019-04-11 09:49:35 +0200

    Ho creato il il primo metodo

--- DataCardio.Test/UnitTest1.cs
+++ DataCardio.Test/UnitTest1.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace DataCardio.Test
@@ -11,7 +11,7 @@ namespace DataCardio.Test
         {
             string Eta = "0";
             string Atteso = "ERRORE!!";
-            string Ottenuto = CardioAnalisiLibrary.DataCardio.Met_ContInsDat(Eta);
+            string Ottenuto = CardioAnalisiLibrary.DataCardio.Met_1_MaxMinAllEff(Eta);
             Assert.AreEqual(Atteso, Ottenuto);
         }
         [TestMethod]
@@ -19,8 +19,22 @@ namespace DataCardio.Test
         {
             string Eta = "150";
             string Atteso = "ERRORE!!";
-            string Ottenuto = CardioAnalisiLibrary.DataCardio.Met_ContInsDat(Eta);
+            string Ottenuto = CardioAnalisiLibrary.DataCardio.Met_1_MaxMinAllEff(Eta);
             Assert.AreEqual(Atteso, Ottenuto);
         }
-    }
-}
+        [TestMethod]
+        public void TestMethod4()
+        {
+            string Eta = "err";
+            string Atteso = "ERRORE!!";
+            string Ottenuto = CardioAnalisiLibrary.DataCardio.Met_1_MaxMinAllEff(Eta);
+            Assert.AreEqual(Atteso, Ottenuto);
+        }
+        [TestMethod]
+        public void TestMethod5()
+        {
+            string Eta = "35";
+            string Atteso ="Il valore massimo è 166,5 Il valore minimo è 129,5";
+            string Ottenuto = CardioAnalisiLibrary.DataCardio.Met_1_MaxMinAllEff(Eta);
+            Assert.AreEqual(Atteso, Ottenuto);
+        }
