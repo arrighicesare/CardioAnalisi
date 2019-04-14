9861be56e588bc3a0eea70d91f9e306ac4f9ba65
arrighicesare <46019616+arrighicesare@users.noreply.github.com>
2019-04-14 11:17:38 +0200

    Ho costruito il terzo metodo

--- CardioAnalisiLibrary/DataCardio.cs
+++ CardioAnalisiLibrary/DataCardio.cs
@@ -66,5 +66,42 @@ namespace CardioAnalisiLibrary
             }
             return Risposta;
         }
+        public static string Met_3_CalBruSesEs(string F, string P, string A, string T, string Sesso)
+        {
+            string Risposta = "";
+            double CalorieBruciate = 0;
+            double Frequenza = 0;
+            double Peso = 0;
+            double Età = 0;
+            double Tempo = 0;
+
+            try
+            {
+                Frequenza = Convert.ToDouble(F);
+                Peso = Convert.ToDouble(P);
+                Età = Convert.ToDouble(A);
+                Tempo = Convert.ToDouble(T);
+                if (Sesso != "M" || Sesso != "m" && Sesso != "F" || Sesso != "f")
+                {
+                    Risposta = "ERRORE!!";
+                }
+                else if (Sesso == "M" || Sesso == "m")
+                {
+                    CalorieBruciate = ((Età * 0.2017) + (Peso * 0.199) + (Frequenza * 0.6309) - 55.0969) * Tempo / 4.184;
+                    Risposta = "Le calorie bruciate durante questa sessione di esercizi è " + CalorieBruciate;
+                }
+                else
+                {
+                    CalorieBruciate = ((Età * 0.074) - (Peso * 0.126) + (Frequenza * 0.4472) - 20.4022) * Tempo / 4.184;
+                    Risposta = "Le calorie bruciate durante questa sessione di esercizi è " + CalorieBruciate;
+                }
+            }
+            catch
+            {
+                Risposta = "ERRORE!!";
+            }
+            return Risposta;
+        }
     }
 }
+
