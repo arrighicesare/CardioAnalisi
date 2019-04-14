3f8f97c1cfbf1b8aaf78da6830d244edcc6db550
arrighicesare <46019616+arrighicesare@users.noreply.github.com>
2019-04-14 11:18:50 +0200

    Ho creato il terzo metodo

--- DataCardio.Test/UnitTest1.cs
+++ DataCardio.Test/UnitTest1.cs
@@ -23,7 +23,7 @@ namespace DataCardio.Test
             Assert.AreEqual(Atteso, Ottenuto);
         }
         [TestMethod]
-        public void TestMethod4()
+        public void TestMethod3()
         {
             string Eta = "err";
             string Atteso = "ERRORE!!";
@@ -31,7 +31,7 @@ namespace DataCardio.Test
             Assert.AreEqual(Atteso, Ottenuto);
         }
         [TestMethod]
-        public void TestMethod5()
+        public void TestMethod4()
         {
             string Eta = "35";
             string Atteso ="Il valore massimo è 166,5 Il valore minimo è 129,5";
@@ -41,7 +41,7 @@ namespace DataCardio.Test
 
 
         [TestMethod]
-        public void TestMethod6()
+        public void TestMethod5()
         {
             string CardRip = "0";
             string Atteso = "ERRORE!!";
@@ -49,7 +49,7 @@ namespace DataCardio.Test
             Assert.AreEqual(Atteso, Ottenuto);
         }
         [TestMethod]
-        public void TestMethod7()
+        public void TestMethod6()
         {
             string CardRip = "err";
             string Atteso = "ERRORE!!";
@@ -57,7 +57,7 @@ namespace DataCardio.Test
             Assert.AreEqual(Atteso, Ottenuto);
         }
         [TestMethod]
-        public void TestMethod8()
+        public void TestMethod7()
         {
             string CardRip = "35";
             string Atteso = "Hai un battito Bradicardico";
@@ -65,7 +65,7 @@ namespace DataCardio.Test
             Assert.AreEqual(Atteso, Ottenuto);
         }
         [TestMethod]
-        public void TestMethod9()
+        public void TestMethod8()
         {
             string CardRip = "75";
             string Atteso = "Hai un battito Normale";
@@ -73,11 +73,61 @@ namespace DataCardio.Test
             Assert.AreEqual(Atteso, Ottenuto);
         }
         [TestMethod]
-        public void TestMethod10()
+        public void TestMethod9()
         {
             string CardRip = "115";
             string Atteso = "Hai un battito Tachicardico";
             string Ottenuto = CardioAnalisiLibrary.DataCardio.Met_2_IntValFreCardRip(CardRip);
+            Assert.AreEqual(Atteso, Ottenuto);
+        }
+
+
+        [TestMethod]
+        public void TestMethod10()
+        {
+            string F="err";
+            string P="err";
+            string A="err";
+            string T="err";
+            string Sesso="err";
+            string Atteso = "ERRORE!!";
+            string Ottenuto = CardioAnalisiLibrary.DataCardio.Met_3_CalBruSesEs( F,  P,  A,  T,  Sesso);
+            Assert.AreEqual(Atteso, Ottenuto);
+        }
+        [TestMethod]
+        public void TestMethod11()
+        {
+            string F = "162";
+            string P = "70";
+            string A = "35";
+            string T = "60";
+            string Sesso = "err";
+            string Atteso = "ERRORE!!";
+            string Ottenuto = CardioAnalisiLibrary.DataCardio.Met_3_CalBruSesEs(F, P, A, T, Sesso);
+            Assert.AreEqual(Atteso, Ottenuto);
+        }
+        [TestMethod]
+        public void TestMethod12()
+        {
+            string F = "err";
+            string P = "err";
+            string A = "err";
+            string T = "err";
+            string Sesso = "F";
+            string Atteso = "ERRORE!!";
+            string Ottenuto = CardioAnalisiLibrary.DataCardio.Met_3_CalBruSesEs(F, P, A, T, Sesso);
+            Assert.AreEqual(Atteso, Ottenuto);
+        }
+        [TestMethod]
+        public void TestMethod13()
+        {
+            string F = "162";
+            string P = "70";
+            string A = "35";
+            string T = "60";
+            string Sesso = "M";
+            string Atteso = "Le calorie bruciate durante questa sessione di esercizi è 976.5544933078393881453154875717017208413";
+            string Ottenuto = CardioAnalisiLibrary.DataCardio.Met_3_CalBruSesEs(F, P, A, T, Sesso);
             Assert.AreEqual(Atteso, Ottenuto);
         }
     }
